d5ba26635fcbe891d62448a9e9292444a3a073b4
AlmuUve <avelazqc@gmail.com>
2021-01-20 20:13:22 +0000

    concat modified

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ window.onload = function(){
     
     let excuse = document.getElementById('excuse');
 
-    excuse.innerHTML = excuseElement(who) + ' ' + excuseElement(action) + ' ' + excuseElement(what) + ' ' + excuseElement(when);
-
+    excuse.innerHTML = excuseElement(who).concat(' ', excuseElement(action), ' ', excuseElement(what), ' ', excuseElement(when));
 }
 
 function excuseElement(arr){
